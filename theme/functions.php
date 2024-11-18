e94dcebce4162b1671a858e4121762941f3dee3d
Wilson Mbuthia <wilsonkabatha@gmail.com>
2024-11-18 19:06:54 +0300

    fix

--- theme/functions.php
+++ theme/functions.php
@@ -202,8 +202,3 @@ require get_template_directory() . '/inc/template-functions.php';
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/custom-functions.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
-require get_template_directory() . '/inc/custom-updater.php';
